dd7b68466c7909a40049cc6e6a225750457025e8
Viacheslav Shevchenko <viacheslavshev4enko@gmail.com>
2023-12-18 13:13:44 +0200

    day 9 solution

---
+++ lib/aoc2023/day9.ex
@@ -0,0 +1,46 @@
+defmodule Aoc2023.Day9 do
+  @behaviour Aoc2023
+
+  @impl true
+  def part1(data) do
+    data
+    |> Enum.map(fn list -> list |> Enum.map(&List.last/1) |> Enum.sum() end)
+    |> Enum.sum()
+  end
+
+  @impl true
+  def part2(data) do
+    data
+    |> Enum.map(fn list -> list |> Enum.map(&List.first/1) |> Enum.reduce(&Kernel.-(&1, &2)) end)
+    |> Enum.sum()
+  end
+
+  @impl true
+  def parse_data(path) do
+    path
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.map(fn line -> line |> String.split(" ") |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(fn sequence ->
+      0
+      |> Stream.iterate(&(&1 + 1))
+      |> Enum.reduce_while([sequence], fn _, [sequence | _] = acc ->
+        new_sequence = generate_diff(sequence)
+
+        new_acc = [new_sequence | acc]
+
+        if Enum.uniq(new_sequence) == [0] do
+          {:halt, new_acc}
+        else
+          {:cont, new_acc}
+        end
+      end)
+    end)
+  end
+
+  defp generate_diff([f, s]), do: [s - f]
+
+  defp generate_diff([f, s | t]) do
+    [s - f | generate_diff([s | t])]
+  end
+end
